22a4c6c0d22c48cee7bd4b4f698d741375061163
noecor <noeliacoronelg@hotmail.com>
2020-11-02 22:49:55 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,42 @@
+// JavaScript
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function accordion_click(id) {
+  var acc = document.getElementById(id);
+  console.log(id);
+  console.log(acc.style.display);
+  if(acc.style.display==""){
+    console.log("display="+acc.style.display);
+    acc.style.display = "Block";
+  } else {
+    acc.style.display = "";
+  }
+  
+}
