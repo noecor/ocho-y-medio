757b56b97ae6ebfcfefba9e9b8da292ca02d614c
noecor <noeliacoronelg@hotmail.com>
2020-11-05 03:02:00 -0300

    add more css

--- main.js
+++ main.js
@@ -4,20 +4,24 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -29,30 +33,26 @@ function showSlides(n) {
 }
 
 function accordion_click(id) {
-  var el_panel = document.getElementById("panel-" + id);
-  var el_arrow = document.getElementById("arrow-" + id);
-  var el_barra = document.getElementById("barra-" + id);
-  var el_panel_barra = document.getElementById("panel-barra-" + id);
+  let el_panel = document.getElementById("panel-" + id);
+  let el_arrow = document.getElementById("arrow-" + id);
+  let el_barra = document.getElementById("barra-" + id);
+  let el_panel_barra = document.getElementById("panel-barra-" + id);
+  let el_accordion = document.getElementById("accordion-" + id);
 
-  console.log(el_panel);
-  console.log(el_arrow);
-  console.log(el_barra);
-  
-
-  if(el_panel.style.display==""){
-    el_panel.classList.toggle("panel-active");
+  if (el_panel.className == "panel") {
+    el_panel.className = "panel-active";
     el_arrow.innerHTML = "&#65087;";
-    el_arrow.classList.toggle("accordion-button-arrow-active");
-    el_barra.classList.toggle("accordion-barra-active");
-    el_panel_barra.classList.toggle("accordion-panel-barra-active");
-
+    el_arrow.className = "accordion-button-arrow-active";
+    el_barra.className = "accordion-barra-active";
+    el_panel_barra.className = "accordion-panel-barra-active";
+    el_accordion.className = "accordion-active";
   } else {
-    el_panel.classList.toggle("panel");
+    el_panel.className = "panel";
     el_arrow.innerHTML = "&#65088;";
-    el_arrow.classList.toggle("accordion-button-arrow");
-    el_barra.classList.toggle("accordion-barra-active");
-    el_panel_barra.classList.toggle("accordion-panel-barra");
+    el_arrow.className = "accordion-button-arrow";
+    el_barra.className = "accordion-barra";
+    el_panel_barra.className = "accordion-panel-barra";
+    el_accordion.className = "accordion";
   }
-  
 }
 //this.classList.toggle("active");
